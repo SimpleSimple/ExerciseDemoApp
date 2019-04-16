a5744d819354602b50af3c1a4c1b9ee899f8900a
playitouch@163.com <playitouch@163.com>
2019-04-16 23:06:44 +0800

    Init Commit

---
+++ ExerciseDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    /*
+     * 委托与事件
+     * 
+     * 
+     */
+    //public delegate void DoSth(string msg);
+    class Program
+    {
+        delegate void DoSth(string msg);
+        static void Main(string[] args)
+        {
+            DoSth doSth = new DoSth(SayHello);
+            doSth("Hello, Jimmy");
+        }
+
+        static void SayHello(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ FileWatcherDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherDemo
+{
+    public delegate void FileMonitorHandler();
+
+    class Program
+    {
+        public event FileMonitorHandler FileMonitorEvent;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ReadFile("config.txt");
+        }
+
+        /// <summary>
+        /// 读取文件，练习读取配置文件操作
+        /// </summary>
+        /// <param name="path"></param>
+        private void ReadFile(string configPath)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + configPath;
+            Console.WriteLine("读取配置文件路径：" + path);
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while (null != (line = reader.ReadLine()))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+}
